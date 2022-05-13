7687bc630f925d812dc592cf41ba57f25b4bd792
motsamaiteboho <96079592+motsamaiteboho@users.noreply.github.com>
2022-05-13 19:37:31 +0200

    what i have do

--- matchingdays-dom.js
+++ matchingdays-dom.js
@@ -2,48 +2,58 @@ const firstDate = document.getElementById("first");
 const secondDate = document.getElementById("second");
 const Elements = document.querySelectorAll(".day");
 
-const weekdays = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let day1 = "";
 let day2 = "";
 
-function fillFirstDate(){
+function fillFirstDate() {
 
     const d = new Date(firstDate.value);
     let day = weekdays[d.getDay()];
     day1 = day;
-    changeColor(day1,day2);
+    changeColor(day1, day2);
 }
-firstDate.addEventListener('change',fillFirstDate)
+firstDate.addEventListener('change', fillFirstDate)
 
-function fillSecondDate(){
+function fillSecondDate() {
     const d = new Date(secondDate.value);
     let day = weekdays[d.getDay()];
     day2 = day;
-    changeColor(day1,day2);
+    changeColor(day1, day2);
 }
-secondDate.addEventListener('change',fillSecondDate)
+secondDate.addEventListener('change', fillSecondDate)
 
-function changeColor(day1,day2) {
-    for(let i = 0; i < Elements.length; i++){
-        Elements[i].classList.remove("color1")
-        Elements[i].classList.remove("color2")
-        Elements[i].classList.remove("color3")
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    changeColor(day1, day2);
+});
+function changeColor(day1, day2) {
 
-    for(let i = 0; i < Elements.length; i++){
-        let item = Elements[i].innerHTML;
-        let newitem = item.toLocaleLowerCase().trim() 
+    // get a reference to the template script tag
+    var templateSource = document.querySelector(".userTemplate").innerHTML;
 
-        if(newitem === day1){
-            Elements[i].classList.add("color1");
-        }
+    // compile the template
+    var userTemplate = Handlebars.compile(templateSource);
 
-        if (newitem=== day2) {
-            Elements[i].classList.add("color2");
-        }
-        if (newitem === day1 && newitem == day2) {
-            Elements[i].classList.add("color3");
-            break;
-        }
+    // get a reference to tableBody where users is to be displayed
+    var userDataElem = document.querySelector(".userData");
+    var userData = {
+        days: weekdays
+    };
+
+    // compile the template
+    var userDataHTML = userTemplate(userData);
+    userDataElem.innerHTML = userDataHTML;
+
+    const colDay1 = document.getElementById(day1);
+    const colDay2 = document.getElementById(day2);
+
+    if(day1 !== day2) {
+        if(day1 !== "")
+            colDay1.classList.add("color1");
+        if(day2 !== "")
+            colDay2.classList.add("color2");
+    }else{
+        if(day1 !== "")
+            colDay1.classList.add('color3');
     }
 }
